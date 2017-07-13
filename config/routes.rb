942360144254c507f9284f569e2a7285a833732a
afeiship <1290657123@qq.com>
2017-07-13 20:56:57 +0800

    issue: 16-Categories-New

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'categories/new'
+
+  get 'categories/create'
+
   root 'posts#index'
   resources :posts
-
+  resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
