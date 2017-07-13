49c28a0ebffcd895dfc1ceda3b4e25f9dbd0a2ea
afeiship <1290657123@qq.com>
2017-07-13 20:54:06 +0800

    issue: 15-Categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170712124214) do
+ActiveRecord::Schema.define(version: 20170713124659) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "title"
+    t.text "description"
+    t.text "sidebar"
+    t.text "submission_text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
