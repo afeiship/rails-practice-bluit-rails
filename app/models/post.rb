9c7b72bf7820484e063773811dfa15369b9084bd
afeiship <1290657123@qq.com>
2017-07-13 21:26:14 +0800

    issue: 17b-Categories-Posts-Migration

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,12 @@
 class Post < ApplicationRecord
+    belongs_to :category
+
     validates :title, length: { maximum: 255 }, presence: true
     enum post_type: [:link, :text]
 
     # presence: 出席 
     validates :link, presence: true, if: :link?
     validates :body, presence: true, if: :text?
+    validates :category_id, presence: true
     
 end
---
+++ db/migrate/20170713130217_add_category_id_to_posts.rb
@@ -0,0 +1,5 @@
+class AddCategoryIdToPosts < ActiveRecord::Migration[5.1]
+  def change
+    add_reference :posts, :category, index: true
+  end
+end
